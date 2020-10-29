f6fcd95fda8267b02d7e0622eaf6a7250c3d8d2d
Jorge Simao <jsimao71@gmail.com>
2020-10-29 17:39:48 +0000

    auto-config mysql profile

--- src/main/java/org/einnovator/sample/movies/MoviesApplication.java
+++ src/main/java/org/einnovator/sample/movies/MoviesApplication.java
@@ -1,7 +1,6 @@
 package org.einnovator.sample.movies;
 
 import org.einnovator.sample.movies.config.AppConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -9,8 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class MoviesApplication {
 
 	public static void main(String[] args) {
-		//new SpringApplicationBuilder(MoviesApplication.class).profiles(AppConfig.getProfiles()).build(args);
-		SpringApplication.run(MoviesApplication.class, args);
+		new SpringApplicationBuilder(MoviesApplication.class).profiles(AppConfig.getProfiles()).run(args);
 	}
 	
 
